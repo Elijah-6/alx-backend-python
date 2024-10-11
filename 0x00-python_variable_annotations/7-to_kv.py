066c46d5274a307121d6de2943661fdfa00a5bef
Elijah Frempong <elijahfrimpong76@gmail.com>
2024-10-11 15:07:53 +0000

    fix:pycodestyle #2203

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -12,4 +12,15 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Converts a key and a value to a tuple where the value is squared.
+
+    Args:
+        k (str): The key.
+        v (Union[int, float]): The value to be squared.
+
+    Returns:
+        Tuple[str, float]: A tuple containing the key and
+        the squared value as a float.
+    """
     return (k, float(v ** 2))
