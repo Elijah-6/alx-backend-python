b03b09d9fe22c081ecc52f216e321b2824896992
Elijah Frempong <elijahfrimpong76@gmail.com>
2024-10-11 15:16:01 +0000

    fix:pycodestyle #2203

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -12,4 +12,14 @@ import math
 
 
 def floor(n: float) -> int:
+    """
+    Returns the largest integer less than or equal to the given float.
+
+    Args:
+        n (float): The float number to be floored.
+
+    Returns:
+        int: The largest integer less than or equal to the given float.
+    """
+
     return math.floor(n)
--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -10,4 +10,14 @@ from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
+    """
+    Calculates the sum of a list of floats.
+
+    Args:
+        input_list (List[float]): A list of floating point numbers.
+
+    Returns:
+        float: The sum of all the numbers in the input list.
+    """
+
     return sum(input_list)
