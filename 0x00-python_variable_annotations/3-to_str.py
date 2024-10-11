cef3ba2bf2c7b2edb6cbb56a8e939e460f941536
Elijah Frempong <elijahfrimpong76@gmail.com>
2024-10-11 15:11:34 +0000

    fix:pycodestyle #2203

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -8,4 +8,13 @@ Functions:
 
 
 def to_str(n: float) -> str:
+    """
+    Converts a floating point number to its string representation.
+
+    Args:
+        n (float): The floating point number to convert.
+
+    Returns:
+        str: The string representation of the floating point number.
+    """
     return str(n)
