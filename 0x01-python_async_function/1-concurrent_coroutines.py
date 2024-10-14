ace2d7f8933d4845cd72b5569447e8775ddc9bca
Elijah Frempong <elijahfrimpong76@gmail.com>
2024-10-14 14:54:12 +0000

    fix pycodestyle

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,25 +1,17 @@
 #!/usr/bin/env python3
-"""  Asynchronously waits for `n` random
-delays and returns them in ascending order.
-"""
-import heapq
+'''Task 1's module.
+'''
+import asyncio
+from typing import List
+
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
-    """
-    Asynchronously waits for `n` random delays and returns them in ascending
-    order.
-
-    Args:
-        n (int): The number of delays to wait for.
-        max_delay (int): The maximum delay value in seconds.
-
-    Returns:
-        list[float]: A list of delays in ascending order.
-    """
-    delays = []
-    for _ in range(n):
-        delay = await wait_random(max_delay)
-        heapq.heappush(delays, delay)
-    return [heapq.heappop(delays) for _ in range(len(delays))]
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''Executes wait_random n times.
+    '''
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
