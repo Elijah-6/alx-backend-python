5d035f38eaa715b4235c6c3d520661d72caf580a
Elijah Frempong <elijahfrimpong76@gmail.com>
2024-10-14 14:56:16 +0000

    fix pycodestyle

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,29 +1,17 @@
 #!/usr/bin/env python3
-"""Asynchronously waits for `n` random delays
-and returns a list of these delays in
-ascending order.
-"""
+'''Task 4's module.
+'''
+import asyncio
+from typing import List
 
 
-import heapq
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list[float]:
-    """
-    Asynchronously waits for `n` random delays
-    and returns a list of these delays in
-    ascending order.
-
-    Args:
-        n (int): The number of delays to wait for.
-        max_delay (int): The maximum delay value (in seconds) for each wait.
-
-    Returns:
-        list[float]: A list of `n` delays in ascending order.
-    """
-    delays = []
-    for _ in range(n):
-        delay = await task_wait_random(max_delay)
-        heapq.heappush(delays, delay)
-    return [heapq.heappop(delays) for _ in range(len(delays))]
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    '''Executes task_wait_random n times.
+    '''
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
